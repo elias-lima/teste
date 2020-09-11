37803ac881f312aaad53e520e20a96df11041df8
elias lima <eliasmoraislima@gmail.com>
2020-09-11 00:58:18 -0100

    return to insert into api postman

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.post('/todos', async (req, res) => {
   try {
       const { description } = req.body;
       const newTodo = await pool.query(
-      "INSERT INTO todo(description) VALUES($1)",
+      "INSERT INTO todo(description) VALUES($1) RETURNING *",
       [description]
       );
 
