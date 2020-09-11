6e8c9d4cce0a00f0fa3e6d4e0400d632b8411453
elias lima <eliasmoraislima@gmail.com>
2020-09-11 00:25:02 -0100

    teste bd postgres

--- server.js
+++ server.js
@@ -3,8 +3,41 @@ const express = require('express');
 const app = express();
  
 // Primeira rota
+const cors = require('cors');
+const pool = require('./db');
+
 app.get('/', (req,res) => {
-res.status(200).send('La API funciona correctamente');
 res.send("Hello World")
 });
-app.listen(3001);
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.listen(5000, () => {
+  console.log("server has started on port 5000");
+
+});
+
+
+//ROUTES//
+
+
+//create a todo
+
+app.post('/todos', async (req, res) => {
+  try {
+      const { description } = req.body;
+      const newTodo = await pool.query(
+      'INSERT INTO todo (description) VALUES($1)',
+      [description]
+      );
+
+      res.json(newTodo);
+  } catch (err){
+    console.error(err.message);
+  }
+});
+
+
+//get all todo
